cede0248b07619d9cef18b083ba222f8cdd08465
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2023-09-21 21:50:08 +0200

    add aws completion

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -49,6 +49,21 @@ Import-Module posh-git
 # npm autocomplete
 Import-Module npm-completion
 
+# aws autocomplete
+Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
+  param($commandName, $wordToComplete, $cursorPosition)
+  $env:COMP_LINE=$wordToComplete
+  if ($env:COMP_LINE.Length -lt $cursorPosition){
+    $env:COMP_LINE=$env:COMP_LINE + " "
+  }
+  $env:COMP_POINT=$cursorPosition
+  aws_completer.exe | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+  Remove-Item Env:\COMP_LINE
+  Remove-Item Env:\COMP_POINT
+}
+
 ## ALIASES
 
 # git alias
